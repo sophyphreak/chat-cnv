4cd1495364dc62d94b26aad69751d9fafb2ec048
Andrew Horn <arhorn@smcm.edu>
2020-07-09 13:10:47 -0500

    initial commit

---
+++ Conversation.go
@@ -0,0 +1,17 @@
+package cnv
+
+import "../msg"
+
+// Conversations is a slice of Conversation
+var Conversations []Conversation
+
+// Conversation is a private conversation between two Users
+type Conversation struct {
+	User1    string        `json:"user1"`
+	User2    string        `json:"user2"`
+	Messages []msg.Message `json:"messages"`
+}
+
+// GetMessageList function
+// 	- checks if Conversation exists with User1 and User2
+//	- returns all messages if exists
